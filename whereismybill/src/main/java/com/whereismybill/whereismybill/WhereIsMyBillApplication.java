5979b3dd55a4236da8e065f2b3c03f52a2e6278c
henriquebenjamim1 <henrique.benjamim19@gmail.com>
2025-09-15 00:05:41 -0300

    adding project

---
+++ whereismybill/src/main/java/com/whereismybill/whereismybill/WhereIsMyBillApplication.java
@@ -0,0 +1,13 @@
+package com.whereismybill.whereismybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsMyBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsMyBillApplication.class, args);
+	}
+
+}
---
+++ whereismybill/src/test/java/com/whereismybill/whereismybill/WhereIsMyBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whereismybill.whereismybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereIsMyBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
